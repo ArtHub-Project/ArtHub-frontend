d3d05163d739a7d7571077123d70734fa817038a
htmlza2555 <P.nopanan@kkumail.com>
2023-11-27 15:09:41 +0700

    Login Endpoint

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,92 @@
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react'
+import { CredentialDTO, LoginDTO, RegisterDTO } from '../types/dto'
+import axios, { AxiosError } from 'axios'
+interface IAuthProviderProps {
+  children: ReactNode
+}
+
+interface IAuthContextType {
+  isLoggedIn: boolean
+  username: string | null
+  login: (username: string, password: string) => Promise<void>
+  registerUser: (newUsername: string, newPassword: string, newName: string) => Promise<void>
+}
+
+const AuthContext = createContext<IAuthContextType | null>(null)
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+
+  if (!context) throw new Error('useAuth must be used within AuthProvider')
+
+  return context
+}
+
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('username')
+
+const checkLoginStatus = async (token: string | null): Promise<boolean> => {
+  if (typeof token !== 'string') return false
+  try {
+    const currentUser = await axios.get('http://localhost:8080/auth/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    if (currentUser.status === 200) return true
+  } catch (err) {
+    if (err instanceof AxiosError && err.response?.status === 403) return false
+  }
+  return false
+}
+
+const AuthProvider = ({ children }: IAuthProviderProps) => {
+  const [isLoggedIn, setIsLoggedin] = useState<boolean>(false)
+  const [username, setUsername] = useState<string | null>(user)
+
+  useEffect(() => {
+    checkLoginStatus(token).then((isLoggedInAlready) => {
+      setIsLoggedin(isLoggedInAlready)
+    })
+  }, [])
+
+  const login = async (username: string, password: string) => {
+    const loginBody: LoginDTO = { username, password }
+
+    try {
+      const res = await axios.post<CredentialDTO>('http://localhost:8080/auth/login', loginBody, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      localStorage.setItem('token', res.data.accessToken)
+      localStorage.setItem('username', username)
+      setIsLoggedin(true)
+      setUsername(username)
+    } catch (err) {
+      throw new Error('Invalid username or password')
+    }
+  }
+
+  const registerUser = async (newUsername: string, newPassword: string, newName: string) => {
+    const newUser: RegisterDTO = {
+      username: newUsername,
+      password: newPassword,
+      name: newName,
+    }
+
+    try {
+      await axios.post<RegisterDTO>('http://localhost:8080/user', newUser, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return <AuthContext.Provider value={{ isLoggedIn, username, login, registerUser }}>{children}</AuthContext.Provider>
+}
+
+export default AuthProvider
