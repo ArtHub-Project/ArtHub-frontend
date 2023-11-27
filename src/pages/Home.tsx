dca73f4fbb0ee11bcb38a91266f49713b8020747
Peeranut Chaturong <annmalaew@Peeranuts-MacBook-Pro.local>
2023-11-27 22:44:21 +0700

    ci/workflows

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import Banner from '../components/Banner'
 import Menu from '../components/Menu'
-
+import Navbar from '../components/Navbar'
 import ProductList from '../components/ProductList'
 
 const Home = () => {
   return (
     <div className="bg-inherit">
+      <Navbar />
       <Menu />
       <Banner />
       <ProductList />
