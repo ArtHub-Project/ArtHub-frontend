de2b686d508d23c58e95fe475ae0b0fe3695d8ae
Peeranut Chaturong <annmalaew@Peeranuts-MacBook-Pro.local>
2023-11-28 19:27:51 +0700

    button/create

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,28 @@ import Checkout from './pages/Checkout'
 import { useAuth } from './providers/AuthProvider'
 import GuardedRoute from './guard/GuardedRoute'
 import Create from './pages/Create'
+import Navbar from './components/Navbar'
 
 function App() {
   const { isLoggedIn } = useAuth()
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route element={<GuardedRoute isRouteAccessible={!isLoggedIn} redirectRoute="/" />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/create" element={<Create />} />
-      </Route>
-      <Route element={<GuardedRoute isRouteAccessible={isLoggedIn} redirectRoute="/" />}>
-        <Route path="/product" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<GuardedRoute isRouteAccessible={!isLoggedIn} redirectRoute="/" />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<Create />} />
+        </Route>
+        <Route element={<GuardedRoute isRouteAccessible={isLoggedIn} redirectRoute="/" />}>
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,14 +19,14 @@ const Navbar = () => {
         <input
           type="text"
           placeholder="Search?"
-          className="input w-96 bg-slate-50 focus:border-[#CF1CB6] border-transparent "
+          className="input w-64 bg-slate-50 focus:border-[#CF1CB6] border-transparent "
         />
         <div className="absolute top-0 bottom-0 left-0"></div>
       </div>
       <div className="space-x-4">
         <Link to="/login">
           <button className="btn bg-slate-50 hover:bg-fuchsia-100 border-hidden">
-            <img src="/images/Moon.svg" />
+            <img src="/images/arrow-left-on-rectangle.svg" />
           </button>
         </Link>
         <Link to="/register">
@@ -36,30 +36,30 @@ const Navbar = () => {
         </Link>
         <Link to="/cart">
           <button className="btn bg-slate-50 hover:bg-fuchsia-100 border-hidden" title="cart">
-            <img src="/images/ShoppingCart.svg" />
+            <img src="/images/shopping-cart.svg" />
           </button>
         </Link>
-        {isLoggedIn ? (
-          <div>
+        {!isLoggedIn ? (
+          <div className="flex space-x-4">
             <button onClick={logout} className="btn bg-slate-50 hover:bg-fuchsia-100 border-hidden" title="cart">
-              Logout
+              <img src="/images/arrow-right-on-rectangle.svg" />
             </button>
+            <div>
+              <Link to="/create">
+                <button className="btn bg-slate-50 hover:bg-fuchsia-100 border-hidden">
+                  <img src="/images/Add2.svg" />
+                </button>
+              </Link>
+            </div>
+            <div className="avatar">
+              <div className="w-12 rounded-3xl">
+                <img src="/images/sunturs/suntur1.jpeg" />
+              </div>
+            </div>
           </div>
         ) : (
-          <div>
-            <div className="text-primary-50">
-              <p>annmalaew</p>
-            </div>
-            <div className="text-xs">
-              <p>@annmalaew</p>
-            </div>
-          </div>
+          <div></div>
         )}
-        <div className="avatar">
-          <div className="w-12 rounded-3xl">
-            <img src="/images/sunturs/suntur1.jpeg" />
-          </div>
-        </div>
       </div>
     </nav>
   )
