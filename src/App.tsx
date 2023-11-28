b5975f7c37dd7edf99aa4acb79212d76b3f21143
Peeranut Chaturong <annmalaew@Peeranuts-MacBook-Pro.local>
2023-11-28 15:11:49 +0700

    guard/guarded route

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,24 @@ import Register from './pages/Register'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
+import { useAuth } from './providers/AuthProvider'
+import GuardedRoute from './guard/GuardedRoute'
 
 function App() {
+  const { isLoggedIn } = useAuth()
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
+      <Route element={<GuardedRoute isRouteAccessible={!isLoggedIn} redirectRoute="/" />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+      <Route element={<GuardedRoute isRouteAccessible={isLoggedIn} redirectRoute="/" />}>
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
     </Routes>
   )
 }
